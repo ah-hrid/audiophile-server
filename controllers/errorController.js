2081931e92b76a40c9ea70d45fd0f60762420bbb
Salma <Hajar&Salma@gmail.com>
2023-12-30 10:16:46 +0100

    debugging

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -43,7 +43,7 @@ module.exports = (error, req, res, next) => {
         const statusCode = error.statusCode || 500;
         const message = error.message || 'server error';
 
-        res.status(400).json({ message, success: false, stackError: error.stack });
+        res.status(statusCode).json({ message, success: false, stackError: error.stack });
 
     } else if (process.env.NODE_ENV === 'production') {
 
